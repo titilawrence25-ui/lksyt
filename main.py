7fec7032fc4bff8d21340f00f65f7ad2c7765043
titilawrence25-ui <titilawrence25@gmail.com>
2026-08-18 13:36:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#luetaan syöte
+r = float(input("anna ympyrän säde:"))
+
+#Laskutoimitus
+A = 3.141 * r**2;
+
+#Lopputulos näytölle
+print(f"Pinta-ala on {A}")
---
+++ teht_2.2.py
@@ -0,0 +1,10 @@
+from re import Math
+import math
+#luetaan syöte
+r = float(input("anna ympyrän säde:"))
+
+#Laskutoimitus
+A = math.pi * r**2;
+
+#Lopputulos näytölle
+print(f"Pinta-ala on {A}")
