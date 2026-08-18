0f894d57e18ac1d8cde09b70ef4ac9cd548fc6e8
titilawrence25-ui <titilawrence25@gmail.com>
2026-08-18 17:04:54 +0300

    kirjoitettu lisää

--- teht_2.3.py
+++ teht_2.3.py
@@ -1,2 +1,7 @@
 # kirjoita uudestaan koodi
-
+kanta = float(input("Suorakulmion kanta:"))
+Korkeus = float(input("Suorakulmion Korkeus:"))
+A = kanta*Korkeus
+P = 2* kanta+Korkeus
+print(A)
+print(P)
